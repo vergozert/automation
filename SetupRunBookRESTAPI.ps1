4c841246093dbc2e7953cbb082b79611579e87b0
Remco Vergoossen <Remco.Vergoossen@valid.nl>
2019-12-11 22:42:39 +0100

    init

---
+++ SetupRunBookRESTAPI.ps1
@@ -0,0 +1,11 @@
+﻿Install-Module AzureAutomationAuthoringToolkit
+
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+$loginresults=Login-AzureRmAccount -ServicePrincipal -Tenant $connection.TenantID `
+-ApplicationId $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
+$context = Get-AzureRmContext
+$SubscriptionId = $context.Subscription
+$cache = $context.TokenCache
+$cacheItem = $cache.ReadItems()
+$AccessToken=($cacheItem | Where-Object { $_.Resource -eq “https://management.core.windows.net/" })[0].AccessToken
+Write-Output -InputObject @{AccessToken = $AccessToken; SubscriptionId = $SubscriptionId }
